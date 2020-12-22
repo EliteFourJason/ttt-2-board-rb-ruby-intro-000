344374dd311efa4827d7705c9935e90d8d665228
Jason Sevilla <jesevillaescobar@gmail.com>
2020-12-22 23:24:04 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,32 +1,23 @@
-row = [" "," "," "]
+row = [" ", " ", " "]
 
-# Position 1: Left
 row[0]
 
-# Position 2: Middle
 row[1]
 
-# Position 3: Right
 row[2]
 
-row = [" "," "," "]
+row = [" ", " ", " "]
 
-# Position 1: Left
 row[4]
 
-# Position 2: Middle
 row[4]
 
-# Position 3: Right
 row[6]
 
-row = [" "," "," "]
+row = [" ", " ", " "]
 
-# Position 1: Left
 row[7]
 
-# Position 2: Middle
 row[8]
 
-# Position 3: Right
 row[9]
